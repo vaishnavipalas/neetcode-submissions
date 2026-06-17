b0b49f72b27f5e9e9ed66caf42c142af732d9d25
Vaishnavi Palasamudrum <148693382+vaishnavipalas@users.noreply.github.com>
2026-06-17 18:19:44 -0400

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,57 @@
+from collections import Counter
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        '''
+        understand -
+        input - two strings: s1 and s2
+        output - boolean
+
+        if s1 is in s2 as a permutation, we return true
+        else we return false
+        can be in any order (permutation)
+        must be consecutive (no chars in between)
+
+        match - sliding window - fixed size
+
+        implement -
+        edge case: if len(s2) < len(s1), return false
+        init left pointer
+        keep track of the current window with a set - start with up to index len(s1)
+        get the frequencies of s1 - goal is to compare them to each substring of
+        s2 that we check to see if the permutation exists - use library
+        iterate through s2 from len(s1)
+        remove the left most, add the current char
+        compare the frequencies
+        if not equal, move on, otherwise return true
+
+        if we go through the whole thing and can't match, then return false
+        
+
+        '''
+        len_s1 = len(s1)
+        len_s2 = len(s2)
+
+        if len_s2 < len_s1:
+            return False
+
+        freqs_s1 = Counter(list(s1))
+        l = 0
+        freqs_s2 = Counter(list(s2[l:len_s1-1]))
+        print(freqs_s2)
+
+        for r in range(len_s1-1, len_s2):
+            freqs_s2[s2[r]] += 1
+            print(freqs_s2)
+            if freqs_s2 == freqs_s1:
+                return True
+            
+            freqs_s2[s2[l]] -= 1
+            if freqs_s2[s2[l]] <= 0:
+                freqs_s2.pop(s2[l])
+                
+            l += 1
+        
+        return False
+        
